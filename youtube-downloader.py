7444185d068e78e8fc637fba338d7df675c6a516
vogel2 <48958459+vogel2@users.noreply.github.com>
2021-01-03 11:51:44 +0200

    4 - Filter by MP4 extension

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -10,5 +10,5 @@ print(f"Views:\n{video.views} \n")
 print(f"Rating:\n{video.rating} / 5.0 \n")
 print(f"Length:\n{video.length/60} minutes \n")
 
-for stream in video.streams.filter(progressive=True):
+for stream in video.streams.filter(progressive=True, file_extension='mp4'):
         print(stream)
