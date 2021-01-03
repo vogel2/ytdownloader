c9ec67580b53820e798ec4dc5aaa15b7e3abc8cc
vogel2 <48958459+vogel2@users.noreply.github.com>
2021-01-03 13:04:32 +0200

    7 - Progress bar and refactoring

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -8,25 +8,32 @@ print(f"Title:\n{video.title} \n")
 print(f"Description:\n{video.description} \n")
 print(f"Views:\n{video.views} \n")
 print(f"Rating:\n{video.rating} / 5.0 \n")
-print(f"Length:\n{video.length/60} minute(s) \n")
+print(f"Length:\n{video.length} / 60 minute(s) \n")
 
-for stream in video.streams.filter(progressive=True, file_extension='mp4'):
-        print(stream)
+# for stream in video.streams.filter(progressive=True, file_extension='mp4'):
+#         print(stream)
 
-quality = str("")
-print(f"Select Quality: HD / SD")
-input(quality)
+# quality = str("")
+# print(f"Select Quality: HD / SD")
+# input(quality)
 
-downloadLocation = str("")
-print(f"Input your download location")
-input(downloadLocation)
+# downloadLocation = str("")
+# print(f"Input your download location")
 
-if quality == "HD":
-    video.streams.get_highest_resolution().download(output_path=downloadLocation)
-elif quality == "SD":
-    video.streams.get_lowest_resolution().download(output_path=downloadLocation)
+video.streams.get_highest_resolution().download(output_path="/d/Projects/ytdownloader", filename="video")
+# input(downloadLocation)
+
+def show_progress_bar(stream, chunk, file_handle, bytes_remaining):
+         return  # do work
+
+# if quality == "HD":
+#     
+# elif quality == "SD":
+#     video.streams.get_lowest_resolution().download(output_path=downloadLocation)
 
 def finish():
     print("Download Complete!")
+
+video.register_on_progress_callback(show_progress_bar)
 
 video.register_on_complete_callback(finish())
