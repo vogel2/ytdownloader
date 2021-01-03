0c07252deb20e241150f94d54e756284662921a9
vogel2 <48958459+vogel2@users.noreply.github.com>
2021-01-03 11:33:06 +0200

    2 - Youtube object and Video information

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -1,2 +1,12 @@
 from pytube import YouTube
 
+link = input("Type video URL: ")
+
+video = YouTube(link)
+
+print(f"Title:\n{video.title}")
+print(f"Description:\n{video.description}")
+print(f"Views:\n{video.views}")
+print(f"Rating:\n{video.rating}")
+print(f"Length:\n{video.length}")
+
